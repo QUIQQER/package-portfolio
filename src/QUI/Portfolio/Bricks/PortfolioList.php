c7a461e3981a0c9560c0ba06cf4ce720bbf7bd3e
Henning Leutz <leutz@pcsg.de>
2015-11-03 18:04:11 +0100

    portfolio css -> control-background; bricks angefangen

---
+++ src/QUI/Portfolio/Bricks/PortfolioList.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file contains QUI\Portfolio\Bricks\PortfolioList
+ */
+namespace QUI\Portfolio\Bricks;
+
+use QUI;
+
+/**
+ * Class Portfolio
+ * @package QUI\Portfolio\Bricks
+ */
+class PortfolioList extends QUI\Control
+{
+    /**
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+
+
+        return 'sss';
+    }
+}
