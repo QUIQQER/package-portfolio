3a3d65c193e03b70787cf604c0f6f41b0198ce70
Henning Leutz <leutz@pcsg.de>
2017-02-07 14:48:35 +0100

    feat: Randomize der Referenzen; Über die Einstellungen kann der Zufallsbutton ein und ausgeschaltet werden

--- types/list.php
+++ types/list.php
@@ -1,6 +1,7 @@
 <?php
 
 $Portfolio = new QUI\Portfolio\Controls\Portfolio(array(
+    'showRandomButton'           => $Site->getAttribute('quiqqer.portfolio.settings.showRandomButton'),
     'entry-effect'               => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEffect'),
     'entry-width'                => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEntryWidth'),
     'data-qui-options-nopopups'  => $Site->getAttribute('quiqqer.portfolio.settings.portfolioNoPopup'),
