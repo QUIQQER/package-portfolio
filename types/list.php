f7081be47a430d273e110a2b382c3ef8914cad60
Michael <michael.danielczok@gmail.com>
2017-02-21 16:34:58 +0100

    feat: Portofliolist -> Benutzer kann einstellen, wie die Bilder angezeigt werden sollen: zentriert (standard) oder gefüllt.

--- types/list.php
+++ types/list.php
@@ -4,6 +4,7 @@ $Portfolio = new QUI\Portfolio\Controls\Portfolio(array(
     'showRandomButton'           => $Site->getAttribute('quiqqer.portfolio.settings.showRandomButton'),
     'entry-effect'               => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEffect'),
     'entry-width'                => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEntryWidth'),
+    'img-position'               => $Site->getAttribute('quiqqer.portfolio.settings.imgPosition'),
     'data-qui-options-nopopups'  => $Site->getAttribute('quiqqer.portfolio.settings.portfolioNoPopup'),
     'data-qui-options-popuptype' => $Site->getAttribute('quiqqer.portfolio.settings.portfolioPopup.type'),
     'Site'                       => $Site
