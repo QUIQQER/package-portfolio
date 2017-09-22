8cb0654f243c5a6e5d2ecb5e938c91ff0548565b
Henning Leutz <leutz@pcsg.de>
2017-09-22 06:18:03 +0200

    fix: reference start number fixed

--- types/list.php
+++ types/list.php
@@ -1,15 +1,16 @@
 <?php
 
 $Portfolio = new QUI\Portfolio\Controls\Portfolio(array(
-    'showRandomButton'             => $Site->getAttribute('quiqqer.portfolio.settings.showRandomButton'),
-    'entry-effect'                 => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEffect'),
-    'entry-width'                  => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEntryWidth'),
-    'img-position'                 => $Site->getAttribute('quiqqer.portfolio.settings.imgPosition'),
-    'data-qui-options-nopopups'    => $Site->getAttribute('quiqqer.portfolio.settings.portfolioNoPopup'),
-    'data-qui-options-popuptype'   => $Site->getAttribute('quiqqer.portfolio.settings.portfolioPopup.type'),
-    'data-qui-options-lazyloading' => true,
-    'data-qui-options-useanchor'   => true,
-    'Site'                         => $Site
+    'showRandomButton'                 => $Site->getAttribute('quiqqer.portfolio.settings.showRandomButton'),
+    'entry-effect'                     => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEffect'),
+    'entry-width'                      => $Site->getAttribute('quiqqer.portfolio.settings.portfolioEntryWidth'),
+    'img-position'                     => $Site->getAttribute('quiqqer.portfolio.settings.imgPosition'),
+    'data-qui-options-nopopups'        => $Site->getAttribute('quiqqer.portfolio.settings.portfolioNoPopup'),
+    'data-qui-options-popuptype'       => $Site->getAttribute('quiqqer.portfolio.settings.portfolioPopup.type'),
+    'data-qui-options-start-reference' => $Site->getAttribute('quiqqer.portfolio.settings.referenceStartNumber'),
+    'data-qui-options-lazyloading'     => true,
+    'data-qui-options-useanchor'       => true,
+    'Site'                             => $Site
 ));
 
 $Engine->assign('Portfolio', $Portfolio);
