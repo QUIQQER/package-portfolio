5c1941f8a55dc0c03d5199ac760f6ea62891112f
Henning Leutz <leutz@pcsg.de>
2019-02-01 13:24:17 +0100

    fix: NameStorm/Website#44

--- ajax/getReference.php
+++ ajax/getReference.php
@@ -13,16 +13,16 @@ QUI::$Ajax->registerFunction(
         $Project = QUI::getProjectManager()->decode($project);
         $Site    = $Project->get($siteId);
 
-        $Control = new QUI\Portfolio\Controls\Reference(array(
+        $Control = new QUI\Portfolio\Controls\Reference([
             'Site' => $Site
-        ));
+        ]);
 
-        $imageList = array();
+        $imageList = [];
         $images    = $Control->getImages();
 
         /* @var $Image QUI\Projects\Media\Image */
         foreach ($images as $Image) {
-            $imageList[] = array(
+            $imageList[] = [
                 'id'           => $Image->getId(),
                 'name'         => $Image->getAttribute('name'),
                 'title'        => $Image->getAttribute('title'),
@@ -30,7 +30,7 @@ QUI::$Ajax->registerFunction(
                 'image_height' => $Image->getAttribute('image_height'),
                 'image_width'  => $Image->getAttribute('image_width'),
                 'url'          => $Image->getUrl(true)
-            );
+            ];
         }
 
         $url = false;
@@ -38,15 +38,15 @@ QUI::$Ajax->registerFunction(
             $url = $Site->getAttribute('quiqqer.portfolio.settings.website');
 
             if (strpos($url, 'http://') === false && strpos($url, 'https://') === false) {
-                $url = 'http://' . $url;
+                $url = 'http://'.$url;
             }
         }
 
-        return array(
-            'content' => $Site->getAttribute('content'),
+        return [
+            'content' => QUI\Output::getInstance()->parse($Site->getAttribute('content')),
             'url'     => $url,
             'images'  => $imageList
-        );
+        ];
     },
-    array('project', 'siteId')
+    ['project', 'siteId']
 );
